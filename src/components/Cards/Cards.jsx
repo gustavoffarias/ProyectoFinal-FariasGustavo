abff1428a02a99693ec3c501f4ac937ad53fd1aa
Gustvo Farias <gustavo.f.farias@hotmail.com>
2024-06-30 12:08:22 -0300

    preentrega1.2

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
 const Cards = () => {
-    const cadsContainer = document.getElementById("cadsContainer")
-    const productCard = document.createElement("div");
-    productCard.innerHTML = `
-        <img src="" class="" alt="">
-        <div class="">
-            <h5 class="">Prueba1</h5>
-            <h6>Prueba2</h6>
-            <span>Prueba3</span><br>
-            <span>Prueba4</span><br>
-            <button class="" id="" style="width: 100%; margin-top:5%;" >Comprar</button>
-        </div>`;
-
-        root.appendChild(productCard);
+    return(
+        <div className='cards'>
+            <img src='./src/assets/img/AceiteMineral1003.png' alt="" />
+            <h2>Titulo del producto</h2>
+            <p>Descripcion del producto</p>
+        </div>
+    )
 }
 
 export default Cards;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,21 +1,21 @@
-const ItemListContainer = ({greeting}) => {
-    const cadsContainer = document.getElementById("cadsContainer")
-    const productCard = document.createElement("div");
-    productCard.innerHTML = `
-        <img src="" class="" alt="">
-        <div class="">
-            <h5 class="">Prueba1</h5>
-            <h6>Prueba2</h6>
-            <span>Prueba3</span><br>
-            <span>Prueba4</span><br>
-            <button class="" id="" style="width: 100%; margin-top:5%;" >Comprar</button>
-        </div>`;
+import Cards from '../Cards/Cards.jsx'
 
-    root.appendChild(productCard);
-        
+const ItemListContainer = ({greeting}) => {
     return (
         <div>
             <h1>{greeting}</h1>
+            <div id='cardsContainer'>
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+                <Cards />
+            </div>
         </div>
     )
 }
